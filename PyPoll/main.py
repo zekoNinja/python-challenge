41fa9a71b33dc01204025ecf1866756a6f8aa5fd
zekoNinja <141674216+zekoNinja@users.noreply.github.com>
2023-09-12 15:45:29 -0400

    Final Submission-Finally

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,11 +2,14 @@
 import os
 import csv 
 
+
 total_votes=0
+#list for unique candidate name
 unique_canadidate=[]
+#creatign a dictionary to store both keys and values in it (keys: candiadate name, values: count for each candidate)
 unique_total_votes={}
-dic={}
-Total_Votes_list=0
+
+
 
 #Storing the file path associated with the csv file
 election_data_csv = "PyPoll\\Resources\\election_data.csv"
@@ -25,78 +28,76 @@ with open(election_data_csv,'r') as csvfile2:
         total_votes = total_votes + 1
 
         candidate_name = row1[2]
-        
-        
-
+        # getting each unique candidate name
         if candidate_name not in unique_canadidate:
             unique_canadidate.append(candidate_name)
-            #reseting vote for each unique canadiadate to 1
+            #reseting vote for each unique canadiadate to 1 and adding it into the dicationary
             unique_total_votes[candidate_name]=1
         else: 
+            #counting the votes for each unique Candidate and adding it into the dicationary 
             unique_total_votes[candidate_name]+=1
        
+    #printing the results
+    print("Election Results\n")
+    print("-------------------\n")
+    print(f"Total Votes: {total_votes}\n")
+    print("-------------------\n")
+
             
 
-   
-           
-
-
-
-# print(unique_total_votes)
-
-#still m,
-maxpercent=float('-inf')
-winner="medo"
-
-
-
+#inializating my variables
 percent =0
+winner=0
 
-#ok
+
+
+#looping through the dicationry to print the results and the winner
 for i,k in unique_total_votes.items():
-    #ok
+    #calculating percent votes
     percent= round(k/total_votes*100,3)
-
-    # if percent > maxpercent:
-    #         winner=i
-            
-
-    print(f" {i} : {percent}% & {k}")
-
-
-# print(winner)
    
-
-
-                
-
-        
-    # print(unique_canadidate)
-                
-
-        
-        
+   
+    #using the max function to find the correpsonding key to the highest value
+    winner=max(unique_total_votes, key=unique_total_votes.get)
+   
+   #printing candidate name percent and total votes for each candidate
+    print(f" {i} : {percent}%  ({k})\n")
 
 
 
+#print the winner
+print("-------------------------\n")
+print(f"Winner: {winner}\n")
+print("-------------------------\n")
 
 
 
-        # election_data_list = row1
-
-        # election_list_final.append(election_data_list)
-
-        # County.append(row1[1])
-        # Candidate.append(row1[2])
-
-
-        # # Elecetion_list=zip(Ballot_id,County,Candidate)
-        # for row2 in election_list_final:
-        #     print(len(election_list_final))
-        # break
-    # print("Election Results\n")
-    # print("-------------------\n")
-    # print(f"Total Votes: {total_m}\n")
-    # print("-------------------\n")
 
     
+#setting the file path for the text file
+text_file_path="PyPoll\\Analysis\\Analysis.txt"
+#opening and writing into the text file
+with open(text_file_path,'w') as text_result2:
+
+    print("Election Results\n",file=text_result2)
+    print("-------------------\n",file=text_result2)
+    print(f"Total Votes: {total_votes}\n",file=text_result2)
+    print("-------------------\n",file=text_result2)
+
+   #looping through the dicationry to print the results and the winner
+    for i,k in unique_total_votes.items():
+        #calculating percent votes
+        percent= round(k/total_votes*100,3)
+   
+   
+    #using the max function to find the correpsonding key to the highest value
+        winner=max(unique_total_votes, key=unique_total_votes.get)
+   
+    #printing candidate name percent and total votes for each candidate
+        print(f" {i} : {percent}%  ({k})\n",file=text_result2)
+
+    #print the winner in the text file    
+    print("-------------------------\n",file=text_result2)
+    print(f"Winner: {winner}\n",file=text_result2)
+    print("-------------------------\n",file=text_result2)
+   
